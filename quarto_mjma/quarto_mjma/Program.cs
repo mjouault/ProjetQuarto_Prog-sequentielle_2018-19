cc2e020a8b431badcb35d1dfa6a18d7c6010a861
maelle64 <mabadie003@ensc.fr>
2018-12-05 15:43:59 +0100

    ajout de la solution

---
+++ quarto_mjma/quarto_mjma/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quarto_mjma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
