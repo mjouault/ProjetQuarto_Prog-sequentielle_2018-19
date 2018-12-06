7ae22c2dd28411e1211f9100646972f154a92ca1
mjouault <mjouault@ensc.fr>
2018-12-06 19:04:16 +0100

    Cf description
    -Changement fction AfficherGrille --Chgmt int grille en string grille (plus facile car pb pour 0000)
    -Ajout fct AvoirGrilleRemplie et Gagner -Ajout variable string grille dans presque toutes les fonctions

--- quarto_mjma/quarto_mjma/Program.cs
+++ quarto_mjma/quarto_mjma/Program.cs
@@ -14,101 +14,27 @@ namespace quarto_mjma
             Console.WriteLine("==============================================");
             Console.WriteLine("            VOUS JOUEZ AU QUARTO");
             Console.WriteLine("==============================================\n");
-            
-            int[,] grille = new int[4, 4];
-            
+
+            string[,] grille = new string[4, 4];
+
             InitialiserGrille(grille);
-            AfficherGrille(grille);
+            JouerOrdi(grille);
             Console.ReadKey();
         }
 
-        static void InitialiserGrille(int[,] grille)
+        static void InitialiserGrille(string[,] grille)
         {
-            grille = new int[4, 4];
 
             for (int i = 0; i < 4; i++)
             {
                 for (int j = 0; j < 4; j++)
                 {
-                    grille[i, j] = 0;
+                    grille[i, j] = "    ";
                 }
             }
         }
 
-
-        static void AfficherGrille(int[,] grille)
-        {
-            for (int i = 0; i < 4; i++) //indice ligne
-            {
-
-                Console.WriteLine("      +-----+-----+-----+-----+");
-                Console.Write("   " + i);
-                Console.Write("  |");
-
-                for (int j = 0; j < 4; j++) // i = indice colonne
-                {
-                    
-                            grille[i, j] = 22222;// aucun caractère et pièce non présente
-                    Console.Write(grille[i, j] + "|");
-
-                }
-                Console.Write("\n");// sauter une ligne pour mettre la barre entre chaque case
-            }
-
-            Console.WriteLine("      +-----+-----+-----+-----+");
-            Console.WriteLine("         0     1     2     3");
-        }
-
-
-        static void OrdiJoue()
-        {
-            int col;
-            int ligne;
-            int[,] grille = new int[4, 4];//grille de 4 par 4
-
-                    //cas où l'ordinateur joue
-                    Console.WriteLine("Que choisissez-vous comme pièce pour l'ordinateur?\n 0000= petite, creuse, carrée, noire et 1111=grande, pleine, ronde, blanche\n vous pouvez mixer plusieurs caractères évidemment.");
-                    int pièce = int.Parse(Console.ReadLine());//on récupère la pièce que le joueur choisi pour l'ordi
-                    Random R = new Random();// choisit aléatoirement la ligne et la colonne pour placer le pion
-
-
-                    ligne = R.Next(0, 3);
-                    col = R.Next(0, 3);
-                    while (grille[ligne, col] == 2222)//tant que l'ordi choisi une case où il y a déjà une pièce
-                    {
-                        ligne = R.Next(0, 3);
-                        col = R.Next(0, 3);
-                    }
-                    grille[ligne, col] = pièce; //l'ordi a mis la pièce dans une case vide
-
-        }
-            
-            
-            static void JoueurJoue()
-            {
-                 int ligne, col;
-                 int[,] grille = new int[4, 4];
-                    Random R = new Random();
-                    //cas où le joueur joue
-                    //insérer un code pour faire choisi une pièce à l'ordi, un random sur chaque caractère?
-                    int car1 = R.Next(0, 1);
-                    int car2 = R.Next(0, 1);
-                    int car3 = R.Next(0, 1);
-                    int car4 = R.Next(0, 1);
-                    // faire un code pour être sur de ne pas retomber sur la même pièce qu'avant
-                    int pièce = car1 + car2 + car3 + car4; //code faux ne pas mettre plus car on veut faire une chaine de caractère avec tous ces chiffres
-
-                    Console.WriteLine("Choisir une ligne (entre 0 et 3)");
-                    ligne = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Choisir une ligne (entre 0 et 3)");
-                    col = int.Parse(Console.ReadLine());
-
-                    grille[ligne, col] = pièce;
-
-                    // for (int i=0; i<4; i++) Console.Write( " | " + )
-            }
-
-        static void Choix1erJoueur()
+        static void Choix1erJoueur(string[,] grille)
         {
             int choix = 0;
             do
@@ -116,19 +42,96 @@ namespace quarto_mjma
                 Console.WriteLine("Si vous voulez jouer en 1er tapez [1] sinon tapez [2]");
                 choix = int.Parse(Console.ReadLine());
                 if (choix == 2) // l'ordi joue en premier
-                    OrdiJoue();
+                    JouerOrdi(grille);
                 if (choix == 1)
-                    JoueurJoue();
+                    JouerJoueur();
                 else //on a pas saisi 1 ou 2 
                 {
                     Console.WriteLine("vous n'avez pas choisi [1] ou [2], recommencez");
                     Console.Write("Si vous voulez jouer en 1er tapez [1] sinon tapez [2]");
                     choix = int.Parse(Console.ReadLine());
                 }
-            } while (choix != 2 | choix != 1);
+            }
+            while (choix != 2 | choix != 1);
         }
+
+        static void JouerOrdi(string[,] grille)
+        {
+            do
+            {
+                Console.WriteLine("Que choisissez-vous comme pièce pour l'ordinateur?\n 0000= petite, creuse, carrée, clair et 1111=grande, pleine, ronde, foncee\n vous pouvez mixer plusieurs caractères évidemment.");
+                string piece = Console.ReadLine();//on récupère la pièce que le joueur choisi pour l'ordi
+                Random R = new Random();// choisit aléatoirement la ligne et la colonne pour placer le pion
+
+
+                int ligne = R.Next(0, 3);
+                int col = R.Next(0, 3);
+
+                grille[ligne, col] = piece;
+
+                AfficherGrille(grille, ligne, col, piece);
+            }
+            while (!Gagner() && !AvoirGrilleRemplie());
+
+        }
+
+        static void JouerJoueur()
+        {
+            int ligne, col;
+            string[,] grille = new string[4, 4];
+            Random R = new Random();
+            //cas où le joueur joue
+            //insérer un code pour faire choisi une pièce à l'ordi, un random sur chaque caractère?
+            int car1 = R.Next(0, 1);
+            int car2 = R.Next(0, 1);
+            int car3 = R.Next(0, 1);
+            int car4 = R.Next(0, 1);
+            // faire un code pour être sur de ne pas retomber sur la même pièce qu'avant
+            string piece = (car1 + car2 + car3 + car4).ToString(); 
+
+            Console.WriteLine("Choisir une ligne (entre 0 et 3)");
+            ligne = int.Parse(Console.ReadLine());
+            Console.WriteLine("Choisir une ligne (entre 0 et 3)");
+            col = int.Parse(Console.ReadLine());
+
+            grille[ligne, col] = piece;
+
+            // for (int i=0; i<4; i++) Console.Write( " | " + )
+        }
+
+        static void AfficherGrille(string[,] grille, int ligne, int col, string piece)
+        {
+            for (int i = 0; i < 4; i++) //indice ligne
+            {
+
+                Console.WriteLine("      +----+----+----+----+");
+                Console.Write("   " + i);
+                Console.Write("  |");
+
+                for (int j = 0; j < 4; j++) // i = indice colonne
+                {
+                        Console.Write(grille[i, j] + "|");
+                }
+                Console.Write("\n");// sauter une ligne pour mettre la barre entre chaque case
+            }
+
+            Console.WriteLine("      +----+----+----+----+");
+            Console.WriteLine("         0    1    2    3");
+        }
+
+        static bool Gagner()
+        {
+            return false;
+        }
+
+        static bool AvoirGrilleRemplie()
+        {
+            return false;
+        }
+
+      
     }
 
-    }
+}
 
 
--- quarto_mjma/test de chaque fonction/Program.cs
+++ quarto_mjma/test de chaque fonction/Program.cs
@@ -10,23 +10,23 @@ namespace test_de_chaque_fonction
     {
         static void Main(string[] args)
         {
-            InitialiserGrille(new int[4, 4]);
-            AfficherGrille(new int[4, 4]);
+            string[,] grille = new string[4, 4];
+            InitialiserGrille(grille);
+            JouerOrdi(grille);
         }
-        static void InitialiserGrille(int[,] grille)
+        static void InitialiserGrille(string[,] grille)
         {
-            grille = new int[4, 4];
 
             for (int i = 0; i < 4; i++)
             {
                 for (int j = 0; j < 4; j++)
                 {
-                    grille[i, j] = 0;
+                    grille[i, j] = "    ";
                 }
             }
         }
 
-        static void Choix1erJoueur()
+       static void Choisir1erJoueur(string [,]grille)
         {
             int choix = 0;
             do
@@ -34,10 +34,14 @@ namespace test_de_chaque_fonction
                 Console.WriteLine("Si vous voulez jouer en 1er tapez [1] sinon tapez [2]");
                 choix = int.Parse(Console.ReadLine());
                 if (choix == 2) // l'ordi joue en premier
-                    OrdiJoue();
+                {
+                    JouerOrdi(grille);
+                }
                 if (choix == 1)
-                    JoueurJoue();
-                else //on a pas saisi 1 ou 2 
+                {
+                    JouerJoueur();
+                }
+                else //on n'a pas saisi 1 ou 2 
                 {
                     Console.WriteLine("vous n'avez pas choisi [1] ou [2], recommencez");
                     Console.Write("Si vous voulez jouer en 1er tapez [1] sinon tapez [2]");
@@ -46,37 +50,84 @@ namespace test_de_chaque_fonction
         } while (choix != 2 | choix != 1);
         }
 
-        static void AfficherGrille(int[,] grille)
+        static void JouerOrdi(string[,] grille)
         {
-            Console.WriteLine("Que choisissez-vous comme pièce pour l'ordinateur?\n 0000= petite, creuse, carrée, noire et 1111=grande, pleine, ronde, blanche\n vous pouvez mixer plusieurs caractères évidemment.");
-            int pièce = int.Parse(Console.ReadLine());//on récupère la pièce que le joueur choisi pour l'ordi
-            Random R = new Random();// choisit aléatoirement la ligne et la colonne pour placer le pion
+            do
+            {
+                Console.WriteLine("Que choisissez-vous comme pièce pour l'ordinateur?\n 0000= petite, creuse, carrée, clair et 1111=grande, pleine, ronde, foncee\n vous pouvez mixer plusieurs caractères évidemment.");
+               string piece = Console.ReadLine();//on récupère la pièce que le joueur choisi pour l'ordi.
+                Random R = new Random();// choisit aléatoirement la ligne et la colonne pour placer le pion
 
-        
-            int ligne = R.Next(0, 3);
-            int col = R.Next(0, 3);
+
+                int ligne = R.Next(0, 3);
+                int col = R.Next(0, 3);
+
+                grille [ligne, col]= piece;
+
+                AfficherGrille(grille, ligne, col, piece);
+            }
+            while (!Gagner() && !AvoirGrilleRemplie());
+
+        }
+       
+        static void AfficherGrille(string[,] grille, int ligne, int col, string piece)
+        {
             for (int i = 0; i < 4; i++) //indice ligne
             {
 
-                Console.WriteLine("      +-----+-----+-----+-----+");
+                Console.WriteLine("      +----+----+----+----+");
                 Console.Write("   " + i);
                 Console.Write("  |");
 
                 for (int j = 0; j < 4; j++) // i = indice colonne
                 {
-                if (i==ligne && j==col)
-                    grille[i, j] = pièce;// la pièce est placée là où l'ordinateur veut
-                 else grille[i, j] = 22222;// aucun caractère et pièce non présente
-                           
                 Console.Write(grille[i, j] + "|");
-
                 }
                 Console.Write("\n");// sauter une ligne pour mettre la barre entre chaque case
             }
 
             Console.WriteLine("      +-----+-----+-----+-----+");
             Console.WriteLine("         0     1     2     3");
+            Console.WriteLine("      +----+----+----+----+");
+            Console.WriteLine("         0    1    2    3");
             Console.ReadLine();
+
         }
+
+        //pb d'affichage pour 0000
+        static bool Gagner ()
+        {
+            return false;
+        }
+
+        static bool AvoirGrilleRemplie ()
+        {
+            return false;
+        }
+
+        static void JouerJoueur()
+        {
+            int ligne, col;
+            string[,] grille = new string[4, 4];
+            Random R = new Random();
+            //cas où le joueur joue
+            //insérer un code pour faire choisi une pièce à l'ordi, un random sur chaque caractère?
+            int car1 = R.Next(0, 1);
+            int car2 = R.Next(0, 1);
+            int car3 = R.Next(0, 1);
+            int car4 = R.Next(0, 1);
+            // faire un code pour être sur de ne pas retomber sur la même pièce qu'avant
+            string piece = (car1 + car2 + car3 + car4).ToString();
+
+            Console.WriteLine("Choisir une ligne (entre 0 et 3)");
+            ligne = int.Parse(Console.ReadLine());
+            Console.WriteLine("Choisir une ligne (entre 0 et 3)");
+            col = int.Parse(Console.ReadLine());
+
+            grille[ligne, col] = piece;
+
+            // for (int i=0; i<4; i++) Console.Write( " | " + )
+        }
+
     }
 }
